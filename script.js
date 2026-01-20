c888ef819bd8204893dea888d2f0bcbe5de525a9
Vaishnavi11656 <166276503+Vaishnavi11656@users.noreply.github.com>
2026-01-21 01:05:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const panels = document.querySelector(".panel");
+
+function removeactive(){
+	panels.forEach(panel) => panel.classList.remove("active");
+}
+
+
+	panels.forEach((panel)=>{
+		panel.addEvenetListener("click",()=>{
+			removeactive();
+			panel.classList.add("active");
+			
+		});
+	});
+
